688cad855015f97eb124c774b75e60425abd22f2
Pratham B Shetty <prathamshetty8055@gmail.com>
2026-06-23 16:04:59 +0530

    Implement student registration API

---
+++ e-magazine-backend/controllers/authController.js
@@ -0,0 +1,59 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, usn, email, password } = req.body;
+
+    // Check NMAMIT email
+    if (!email.endsWith("@nmamit.in")) {
+      return res.status(400).json({
+        message: "Only NMAMIT email addresses are allowed",
+      });
+    }
+
+    // Check existing user
+    const existingUser = await User.findOne({
+      $or: [{ email }, { usn }],
+    });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create user
+    const user = await User.create({
+      name,
+      usn,
+      email,
+      password: hashedPassword,
+    });
+
+    const userResponse = {
+  _id: user._id,
+  name: user.name,
+  usn: user.usn,
+  email: user.email,
+  role: user.role,
+  department: user.department,
+};
+
+res.status(201).json({
+  message: "User registered successfully",
+  user: userResponse,
+});
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+module.exports = {
+  registerUser,
+};
--- e-magazine-backend/server.js
+++ e-magazine-backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const authRoutes = require("./routes/authRoutes");
 
 const connectDB = require("./config/db");
 
@@ -12,6 +13,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("E-Magazine Backend Running");
