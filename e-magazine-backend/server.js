58f54f410b84c5c8cef187616529071a48d99f33
Pratham B Shetty <prathamshetty8055@gmail.com>
2026-06-23 16:20:42 +0530

    Add JWT authentication and protected routes

--- e-magazine-backend/server.js
+++ e-magazine-backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const authRoutes = require("./routes/authRoutes");
+const { protect } = require("./middleware/auth");
 
 const connectDB = require("./config/db");
 
@@ -17,6 +18,12 @@ app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("E-Magazine Backend Running");
+});
+app.get("/api/profile", protect, (req, res) => {
+  res.json({
+    message: "Protected Route Accessed",
+    user: req.user,
+  });
 });
 
 const PORT = process.env.PORT || 5000;
