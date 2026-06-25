3c1630cc498704cc0e992296f429844bc58e6467
Pratham B Shetty <prathamshetty8055@gmail.com>
2026-06-25 16:38:53 +0530

    Initialize frontend with Vite and Tailwind CSS

---
+++ e-magazine/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <div className="bg-white p-10 rounded-2xl shadow-xl">
+        <h1 className="text-5xl font-bold text-emerald-600">
+          NMAMIT E-Magazine
+        </h1>
+
+        <p className="mt-4 text-gray-600 text-center">
+          Vite + Tailwind is Working 🚀
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
